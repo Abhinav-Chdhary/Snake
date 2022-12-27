601ed5a06ab40fedbc1e3304e4bbed75ae86515b
Abhinav Choudhary <abhinavchdhary@gmail.com>
2022-12-27 19:21:04 +0530

    Grow And obstacles done

--- Snake/Assets/Scripts/Snake.cs
+++ Snake/Assets/Scripts/Snake.cs
@@ -33,9 +33,42 @@ public class Snake : MonoBehaviour
     }
     private void FixedUpdate()
     {
+        for(int i=_segments.Count-1; i>0;i--)
+        {
+            _segments[i].position = _segments[i-1].position;
+        }
+
         transform.position = new Vector3(
             Mathf.Round(transform.position.x)+direction.x,
             Mathf.Round(transform.position.y)+direction.y,
             0.0f );
+    }
+    private void Grow()
+    {
+        Transform segment = Instantiate( _segmentPrefab );
+        segment.position = _segments[_segments.Count - 1].position;
+
+        _segments.Add( segment );
+    }
+    private void ResetGame()
+    {
+        for(int i=1; i<_segments.Count; i++)
+        {
+            Destroy(_segments[i].gameObject);
+        }
+
+        _segments.Clear();
+        _segments.Add(transform);
+        transform.position = Vector3.zero;
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Food")
+        {
+            Grow();
+        }else if(collision.tag == "Obstacle")
+        {
+            ResetGame();
+        }
     }
 }
