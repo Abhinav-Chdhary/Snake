5f79d2ecc8f4e4c0853c2a92815e2e5ba34c8b4a
Abhinav Choudhary <abhinavchdhary@gmail.com>
2022-12-27 11:01:53 +0530

    Movement And Grid Area Setup

---
+++ Snake/Assets/Scripts/Snake.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public class Snake : MonoBehaviour
+{
+    private Vector2 direction = Vector2.right;
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            direction = Vector2.up;
+        }
+        else if (Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            direction = Vector2.down;
+        }
+        else if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            direction = Vector2.right;
+        }
+        else if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            direction = Vector2.left;
+        }
+    }
+    private void FixedUpdate()
+    {
+        transform.position = new Vector3(
+            Mathf.Round(transform.position.x)+direction.x,
+            Mathf.Round(transform.position.y)+direction.y,
+            0.0f );
+    }
+}
